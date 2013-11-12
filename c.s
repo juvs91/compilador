4803e03fa45c95f7409b45a5f8812820bf68aed4
juvs <juve.guz@gmail.com>
2013-11-12 00:58:33 -0600

    viendo la recursividad

--- c.s
+++ c.s
@@ -1,29 +1,13 @@
 int a;
-int b;
-float f;
+float b;  
+char c;
 
-void uno(int a) {
-    a = a + b * a;
-    print(a, b, a + b);
-}
+int uno(){  
+	return a+8;
 
-void dos(int a, int b, float g) {
-    int i;
-    i = b;
-    loop(i > 0) {
-        a = a + b * i + b;
-        uno(i * 2);
-        print(a);
-        i = i - 1;
-    };
-}
+}            
+main(){
 
-main() {
-    a = 3;
-    b = a + 1;
-    print(a, b);
-    f = 3.14;
-    dos(a + b * 2, b, f * 3); 
-	a = uno(1) + uno(1);
-    print(a, b, f * 2 + 1);
+	a=2;
+	print(a);
 }
